23076e68cb459b4abfe1ce0ac1ac7843b5779d24
Josh Kuhar <jdkuhar@gmail.com>
2016-09-22 15:20:06 -0400

    update

--- promises.js
+++ promises.js
@@ -16,20 +16,66 @@
 //   });
 // });
 
-console.log('Console log after calling wait:', new Date());
+// console.log('Console log after calling wait:', new Date());
 
-var promise = new Promise(function(resolve) {
-  resolve('success');
-});
+// var promise = new Promise(function(resolve) {
+//   resolve('success');
+// });
 
 //console.log(promise);
 
-promise.then(function(result) {
-  console.log(result);
-  return result;
-}).then(function(result) {
-  console.log(result);
-  return result;
-}).then(function(result) {
-  console.log(result);
-});
+// promise.then(function(result) {
+//   console.log(result);
+//   return result;
+// }).then(function(result) {
+//   console.log(result);
+//   return result;
+// }).then(function(result) {
+//   console.log(result);
+// });
+
+// function wait(seconds) { 
+//   return new Promise(function(resolve) {
+//     setTimeout(function(){
+//       resolve(new Date());
+//     }, seconds * 1000);
+//   });
+// }
+
+// console.log('Console log before calling wait:', new Date());
+
+// wait(3).then(function(date) {
+//   console.log('Console log after three seconds:', date);
+//   return wait(1);
+// }).then(function(date) {
+//   console.log('Console log after one second:', date);
+//   return wait(5);
+// }).then(function(date) {
+//   console.log('Console log after five seconds:', date);
+// });
+
+// console.log('Console log after calling wait:', new Date());
+
+async function wait(seconds) {
+  await setTimeout(function(){
+    console.log('Console log after async await:', new Date());
+  }, seconds * 1000);
+}
+
+console.log('Console log before calling wait:', new Date());
+
+wait(3);
+
+console.log('Console log after calling wait:', new Date());
+
+
+
+
+
+
+
+
+
+
+
+
