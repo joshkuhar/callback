2c8b64bbef5d16d394965f9c33f65fd72bcb73bc
Josh Kuhar <jdkuhar@gmail.com>
2016-09-22 15:24:10 -0400

    update

--- asynch.js
+++ asynch.js
@@ -6,16 +6,29 @@ function wait(seconds) {
   });
 }
 
+// console.log('Console log before calling wait:', new Date());
+
+// wait(3).then(function(date) {
+//   console.log('Console log after three seconds:', date);
+//   return wait(1);
+// }).then(function(date) {
+//   console.log('Console log after one second:', date);
+//   return wait(5);
+// }).then(function(date) {
+//   console.log('Console log after five seconds:', date);
+// });
+
+// console.log('Console log after calling wait:', new Date());
+
 console.log('Console log before calling wait:', new Date());
 
-wait(3).then(function(date) {
-  console.log('Console log after three seconds:', date);
-  return wait(1);
-}).then(function(date) {
-  console.log('Console log after one second:', date);
-  return wait(5);
-}).then(function(date) {
-  console.log('Console log after five seconds:', date);
-});
+(async function() {
+  var foo = await wait(3);
+  console.log('Console log after three seconds:', foo);
+  var bar = await wait(1);
+  console.log('Console log after one second:', bar);
+  var baz = await wait(5);
+  console.log('Console log after five seconds:', baz);
+}());
 
 console.log('Console log after calling wait:', new Date());
