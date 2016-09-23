f41e6c3d868ad3955869b95e864671e37e4c79dd
Josh Kuhar <jdkuhar@gmail.com>
2016-09-23 14:29:55 -0400

    update

--- events.js
+++ events.js
@@ -1,49 +1,33 @@
 var events = require('events');
 
 var progressBar = new events.EventEmitter();
-// ORIGNINAL FUNCTION
-// function onStart(number){
-//   console.log(number);
-// }
+
+
 progressBar.on('start', function(number){
 	console.log(number);
 });
-// ORIGINAL FUNCTION
-// function onProgress(number){
-//   if (number % 5 === 0 && number !== 0)
-//     console.log("I'm divisible by 5");
-// }
-progressBar.on('onProgress', function(number){
-	if (number % 5 === 0 && number !== 0){
-		console.log("The above number is divisible by 5");
-	}
-});
-// function onEnd(number, max){
-//   if (number === max){
-//     console.log("I'm done");
-//   }
-// }
 
-// //progress function that takes three callbacks
-// function progress(max, onStart, onProgress, onEnd){
-//   var x = 0;
-//   while(x < max){
-//     x++;
-//     onStart(x);
-//     onProgress(x);
-//     onEnd(x, max);
-//   }
-// }
-// progressBar.on('progress', function(start, onProgress){
-// 	for(var x = 1; x < 11; x++){
-// 		start(x);
-// 		onProgress(x);
-// 	}
-// });
-// //call to progess function
-// progress(15, onStart, onProgress, onEnd);
-// progressBar.emit('start', '10');
-// progressBar.emit('onProgress', 10);
-// progressBar.emit('progress', 'start', 'onProgress');
+progressBar.on('progress', function(one, two){
+	var x = one;
+
+})
+// Can you pass a callback function in as an argument? 
+// And how do you call that callback function?
+progressBar.on('event', function(a, b){
+	console.log(a, b);
+});
+
+progressBar.emit('event', 'a', 'b');
+
+
+
+
+
+
+
+
+
+
+
 
 
--- music-player.js
+++ music-player.js
@@ -1,52 +1,53 @@
-// var MusicPlayer = function() {
-//     this.listeners = {};
-// };
+var MusicPlayer = function() {
+    this.listeners = {};
+};
 
-// MusicPlayer.prototype.on = function(eventName, listener) {
-//     if (!this.listeners.hasOwnProperty(eventName)) {
-//         this.listeners[eventName] = [listener];
-//     }
-//     else {
-//         this.listeners[eventName].push(listener);
-//     }
-// };
+MusicPlayer.prototype.on = function(eventName, listener) {
+    if (!this.listeners.hasOwnProperty(eventName)) {
+        this.listeners[eventName] = [listener];
+    }
+    else {
+        this.listeners[eventName].push(listener);
+    }
+};
 
-// MusicPlayer.prototype.emit = function(eventName) {
-//     if (!this.listeners.hasOwnProperty(eventName)) {
-//         return;
-//     }
+MusicPlayer.prototype.emit = function(eventName) {
+    if (!this.listeners.hasOwnProperty(eventName)) {
+        return;
+    }
 
-//     var args = [];
-//     for (var i=1; i<arguments.length; i++) {
-//         args.push(arguments[i]);
-//     }
+    var args = [];
+    for (var i=1; i<arguments.length; i++) {
+        args.push(arguments[i]);
+    }
 
-//     this.listeners[eventName].forEach(function(listener){
-//         listener.apply(null, args);
-//     });
-// };
+    this.listeners[eventName].forEach(function(listener){
+        listener.apply(null, args);
+    });
+};
 
-// var player = new MusicPlayer();
+var player = new MusicPlayer();
 
-// player.on('start', function(artist, song) {
-//     console.log('Starting audio stream playin', artist, song);
-// });
+player.on('start', function(artist, song) {
+    console.log('Starting audio stream playin', artist, song);
+});
 
-// player.on('stop', function() {
-//     console.log('Stopping audio stream');
-// });
+player.on('stop', function() {
+    console.log('Stopping audio stream');
+});
 
-// player.on('start', function() {
-//     console.log('Updating UI to started state');
-// });
+player.on('start', function() {
+    console.log('Updating UI to started state');
+});
 
-// player.on('stop', function() {
-//     console.log('Updating UI to stopped state');
-// });
+player.on('stop', function() {
+    console.log('Updating UI to stopped state');
+});
 
-// player.emit('start', 'Sleater Kinney', 'No Cities to Love');
-// player.emit('stop');
+player.emit('start', 'Sleater Kinney', 'No Cities to Love');
+player.emit('stop');
 
+console.log('next block of code below.............');
 
 var events = require('events');
 
@@ -70,3 +71,29 @@ player.on('stop', function() {
 
 player.emit('start', 'Sleater Kinney', 'No Cities to Love');
 player.emit('stop');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
